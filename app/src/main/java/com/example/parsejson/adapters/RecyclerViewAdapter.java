cd50f5b4c07a01900cbb143e23fa74e7ef49702f
Yuri Yurkov <yu.yurkov@gmail.com>
2018-07-05 15:21:26 +1000

    add adapter

--- app/src/main/java/com/example/parsejson/adapters/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/parsejson/adapters/RecyclerViewAdapter.java
@@ -2,10 +2,17 @@ package com.example.parsejson.adapters;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.example.parsejson.model.Anime;
+import com.example.parsejson.R;
 
 import java.util.List;
 
@@ -13,19 +20,36 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
     private Context mContext;
     private List<Anime> mData;
+    RequestOptions option;
 
     public RecyclerViewAdapter(Context mContext, List<Anime> mData) {
         this.mContext = mContext;
         this.mData = mData;
+
+        // Options for Glide
+        this.option = new RequestOptions().centerCrop().placeholder(R.drawable.loading_shape).error(R.drawable.loading_shape);
     }
 
     @Override
     public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+
+        View view;
+        LayoutInflater inflater = LayoutInflater.from(mContext);
+        view = inflater.inflate(R.layout.anime_row_item, parent,false);
+
+        return new MyViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(MyViewHolder holder, int position) {
+        holder.tv_name.setText(mData.get(position).getName());
+        holder.tv_rating.setText(mData.get(position).getRating());
+        holder.tv_category.setText(mData.get(position).getCategorie());
+        holder.tv_studio.setText(mData.get(position).getStudio());
+
+        // загрузка изобрадения и присвоение его ImageView
+        Glide.with(mContext).load(mData.get(position).getImage_url()).apply(option).into(holder.img_thumbnail);
+
 
     }
 
@@ -36,8 +60,27 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
     public static class MyViewHolder extends RecyclerView.ViewHolder {
 
+
+        TextView tv_name;
+        TextView tv_rating;
+        TextView tv_category;
+        TextView tv_studio;
+        ImageView img_thumbnail;
+
+
+
         public MyViewHolder(View itemView) {
+
             super(itemView);
+
+            tv_name = itemView.findViewById(R.id.anime_name);
+            tv_rating = itemView.findViewById(R.id.rating);
+            tv_category = itemView.findViewById(R.id.categotrie);
+            tv_studio = itemView.findViewById(R.id.studio);
+            img_thumbnail = itemView.findViewById(R.id.thumbnail);
+
+
+
         }
     }
 }
